6b7ca54835ba9c83692e50f9113c3047955a3bb5
Victor Almeida França <fr4ncacc@gmail.com>
2019-04-08 21:27:03 -0300

    iniciando devesenvolvimento, game.py, instalando dependencias

--- game.py
+++ game.py
@@ -1,39 +1,91 @@
 import pygame
+
+WIDTH = 800
+HEIGHT = 500
+THAY_WIDTH = 60
+THAY_HEIGHT = 40
+THAY_X = 50
+THAY_Y = 80
+THAY_VEL = 3
+PROJETIL_WIDTH = 40
+PROJETIL_HEIGHT = 10
+PROJETIL_X = THAY_X
+PROJETIL_Y = THAY_Y + ((THAY_HEIGHT / 2) - PROJETIL_HEIGHT / 2)
+PROJETIL_VEL = 2
+PROJETIL_IS_MOVING = False
+RUN = True
+
 pygame.init()
-
-window = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption("First game")
-
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 5 
-
-run = True
-while(run):
-  pygame.time.delay(100)
-
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
-
-  keys = pygame.key.get_pressed()
-
-  if keys[pygame.K_LEFT]:
-    x -= vel
-  if keys[pygame.K_RIGHT]:
-    x += vel
-  if keys[pygame.K_DOWN]:
-    y += vel
-  if keys[pygame.K_UP]:
-    y -= vel
-
-  window.fill((0))
-
-  pygame.draw.rect(window, (255, 255, 255), (x, y, width, height))
-  pygame.display.update()
+janela = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("ThayShooter")
 
 
-pygame.quit()
+def run():
+    global RUN
+    global THAY_HEIGHT
+    global THAY_WIDTH
+    global THAY_X
+    global THAY_Y
+    global THAY_VEL
+    global PROJETIL_HEIGHT
+    global PROJETIL_WIDTH
+    global PROJETIL_X
+    global PROJETIL_Y
+    global PROJETIL_VEL
+
+    while RUN:
+        pygame.time.delay(6)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUN = False
+
+        movimentos()
+
+        janela.fill((0))
+
+        pygame.draw.rect(janela, (255, 255, 255),
+                         (THAY_X, THAY_Y, THAY_WIDTH, THAY_HEIGHT))
+        pygame.draw.rect(janela, (255, 255, 255),
+                         (PROJETIL_X, PROJETIL_Y, PROJETIL_WIDTH, PROJETIL_HEIGHT))
+        pygame.display.update()
+
+    pygame.quit()
+
+
+def movimentos():
+    global RUN
+    global THAY_HEIGHT
+    global THAY_WIDTH
+    global THAY_X
+    global THAY_Y
+    global THAY_VEL
+    global PROJETIL_HEIGHT
+    global PROJETIL_WIDTH
+    global PROJETIL_X
+    global PROJETIL_Y
+    global PROJETIL_VEL
+    global PROJETIL_IS_MOVING
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and THAY_X > 0:
+        THAY_X -= THAY_VEL
+    if keys[pygame.K_RIGHT] and THAY_X < WIDTH - THAY_WIDTH:
+        THAY_X += THAY_VEL
+    if keys[pygame.K_DOWN] and THAY_Y < HEIGHT - THAY_HEIGHT:
+        THAY_Y += THAY_VEL
+    if keys[pygame.K_UP] and THAY_Y > 0:
+        THAY_Y -= THAY_VEL
+    if keys[pygame.K_SPACE]:
+        PROJETIL_IS_MOVING = True
+
+    while PROJETIL_IS_MOVING:
+        PROJETIL_X += PROJETIL_VEL
+
+    if keys[pygame.K_ESCAPE]:
+        RUN = False
+
+
+if __name__ == "__main__":
+    run()
