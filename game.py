defdd169f54c42b88adf099df0246f996877c464
Victor Almeida França <fr4ncacc@gmail.com>
2019-04-11 15:53:25 -0300

    classe thay, classe projetil, atirar projetil, refatoraçao

--- game.py
+++ game.py
@@ -1,83 +1,104 @@
 import pygame
 
+# Constantes
+CLOCK = pygame.time.Clock()
+BG = pygame.image.load('bg.jpg')
+THAY_BG = pygame.image.load('thay1.png')
 WIDTH = 800
 HEIGHT = 500
-THAY_WIDTH = 60
-THAY_HEIGHT = 40
-THAY_X = 50
-THAY_Y = 80
-THAY_VEL = 3
-PROJETIL_WIDTH = 40
-PROJETIL_HEIGHT = 10
-PROJETIL_X = THAY_X
-PROJETIL_Y = THAY_Y + ((THAY_HEIGHT / 2) - PROJETIL_HEIGHT / 2)
-PROJETIL_VEL = 4
-PROJETIL_IS_MOVING = False
 RUN = True
 
-pygame.init()
-janela = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("ThayShooter")
+# Classe da jogador
+class Thay():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 3
 
-def run():
+    # Função para desenhar o jogador
+    def draw(self, janela):
+        janela.blit(THAY_BG, (self.x, self.y))
+        
+# Classe do projétil
+class Projetil():
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.vel = 6
+    
+    # Função para desenhar um projetil
+    def draw(self, janela):
+        pygame.draw.circle(janela, self.color, (self.x, self.y), self.radius)
+
+# Função para desenhos na tela
+def drawWindow(janela, ThayNave, projeteis):
+    janela.blit(BG, (0, 0))
+    ThayNave.draw(janela)
+    for projetil in projeteis:
+        projetil.draw(janela)
+    pygame.display.update()
+
+# Função de movimentos e teclas
+def movimentos(ThayNave, projeteis):
     global RUN
-
-    while RUN:
-        pygame.time.delay(6)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                RUN = False
-
-        movimentos()
-
-        janela.fill((0))
-
-        pygame.draw.rect(janela, (255, 255, 255), (THAY_X, THAY_Y, THAY_WIDTH, THAY_HEIGHT))
-        pygame.draw.rect(janela, (255, 255, 255), (PROJETIL_X, PROJETIL_Y, PROJETIL_WIDTH, PROJETIL_HEIGHT))
-        pygame.display.update()
-
-    pygame.quit()
-
-
-def movimentos():
-    global RUN
-    global THAY_X
-    global THAY_Y
-    global PROJETIL_X
-    global PROJETIL_IS_MOVING
 
     keys = pygame.key.get_pressed()
-
 # Movimentos da Thay
     # Move pra esquerda até a margem
-    if keys[pygame.K_LEFT] and THAY_X > 0:
-        THAY_X -= THAY_VEL
+    if keys[pygame.K_LEFT] and ThayNave.x > 0:
+        ThayNave.x -= ThayNave.vel
     # Move pra direita até a margem
-    if keys[pygame.K_RIGHT] and THAY_X < WIDTH - THAY_WIDTH:
-        THAY_X += THAY_VEL
+    if keys[pygame.K_RIGHT] and ThayNave.x < WIDTH - ThayNave.width:
+        ThayNave.x += ThayNave.vel
     # Move pra baixo até a margem
-    if keys[pygame.K_DOWN] and THAY_Y < HEIGHT - THAY_HEIGHT:
-        THAY_Y += THAY_VEL
+    if keys[pygame.K_DOWN] and ThayNave.y < HEIGHT - ThayNave.height:
+        ThayNave.y += ThayNave.vel
     # Move pra cima até a margem
-    if keys[pygame.K_UP] and THAY_Y > 0:
-        THAY_Y -= THAY_VEL
+    if keys[pygame.K_UP] and ThayNave.y > 0:
+        ThayNave.y -= ThayNave.vel
 
 # Movimentos do projétil
+    for projetil in projeteis:
+        if projetil.x < WIDTH:
+            projetil.x += projetil.vel
+        else:
+            projeteis.pop(projeteis.index(projetil))
     # Atira o projétil
-    if not(PROJETIL_IS_MOVING):
-        if keys[pygame.K_SPACE]:
-            PROJETIL_IS_MOVING = True
-    else:
-        PROJETIL_X += PROJETIL_VEL
-
-    # Move o projétil até o final e volta para a posição inicial
-    if PROJETIL_X >= WIDTH:
-        PROJETIL_X = THAY_X
-        PROJETIL_IS_MOVING = False
+    if keys[pygame.K_x]:
+        if len(projeteis) < 50:
+            if len(projeteis) > 0 and projeteis[len(projeteis) - 1].x > ThayNave.x + 60:
+                projeteis.append(Projetil(round(ThayNave.x + ThayNave.width // 2), round(ThayNave.y + ThayNave.height // 2), 3, (255, 0, 0)))
+            elif len(projeteis) == 0: 
+                projeteis.append(Projetil(round(ThayNave.x + ThayNave.width // 2), round(ThayNave.y + ThayNave.height // 2), 3, (255, 0, 0)))
 
     if keys[pygame.K_ESCAPE]:
         RUN = False
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUN = False
+
+# Função de início
+def run(ThayNave, projeteis):
+    global RUN
+
+    janela = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("ThayShooter")
+
+    while RUN:
+        CLOCK.tick(60)
+
+        movimentos(ThayNave, projeteis)
+        drawWindow(janela, ThayNave, projeteis)
+
+    pygame.quit()
 
 if __name__ == "__main__":
-    run()
+    pygame.init()
+    ThayNave = Thay(40, 40, 64, 48)
+    projeteis = []
+    run(ThayNave, projeteis)
