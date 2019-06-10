1cb4a58519b5e518d568e6c5fcfefc78f7053966
Thaynara <thaycic@hotmail.com>
2019-06-10 10:12:15 +0000

    projetao

--- src/Projetil.py
+++ src/Projetil.py
@@ -4,18 +4,20 @@ from pygame import draw, image
 # Classe do projétil
 class Projetil():
 
-    PROJETIL_BG = image.load(path.join('../assets', 'projetil.png'))
-
-    def __init__(self, x, y, radius, color):
+    def __init__(self, x, y, radius, color, isEspecial1):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
         self.vel = 6
+        self.isEspecial1 = isEspecial1
+        self.projetil_bg = image.load(path.join('../assets', 'projetao.png' if self.isEspecial1 else 'projetil.png'))
+        self.dano = 34 if not self.isEspecial1 else 100
+
     
     # Função para desenhar um projetil
     def draw(self, janela):
-        janela.blit(self.PROJETIL_BG, (self.x, self.y))
+        janela.blit(self.projetil_bg, (self.x, self.y))
         #draw.circle(janela, self.color, (self.x, self.y), self.radius)
         self.move()
     
